2edebb28cff1da2f10fb1aed632c382c50993109
Andrey Savostyanyuk <andrey_savostyanyuk@mail.ru>
2017-05-15 19:22:58 +0300

    first commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(function(){"use strict";var wind=$(window);
+$(".navbar-nav").singlePageNav({speed:1e3,currentClass:"active",offset:70});
+wind.on("scroll",function(){var bodyScroll=$(window).scrollTop(),navbar=$(".navbar-default");
+if(bodyScroll>100){navbar.addClass("nav-scroll")}else{navbar.removeClass("nav-scroll")}});
+$(".button-scroll").on("click",function(){var scrollTo=$(this).attr("data-scrollTo");
+$("body, html").animate({scrollTop:$("#"+scrollTo).offset().top-60},1e3)});
+$(".dropdown").on("click",".dropdown-icon",function(){$(this).next().slideDown();
+$(".dropdown-info").not($(this).next()).slideUp()});wind.on("scroll",function(){$(".skills .skills-progress span").each(function(){var bottom_of_object=$(this).offset().top+$(this).outerHeight();
+var bottom_of_window=$(window).scrollTop()+$(window).height();var myVal=$(this).attr("data-value");
+if(bottom_of_window>bottom_of_object){$(this).animate({width:myVal},2e3)}})});
+$(".gallery").isotope({itemSelector:".item-img"});var $gallery=$(".gallery").isotope({});
+$(".filtering").on("click","span",function(){var filterValue=$(this).attr("data-filter");
+$gallery.isotope({filter:filterValue})});
+$(".filtering").on("click","span",function(){$(this).addClass("active").siblings().removeClass("active")});
+$(".client .owl-carousel").owlCarousel({items:1,autoplay:true,loop:true,smartSpeed:500,mouseDrag:false,nav:true,navText:["<",">"],dots:false})});
+$(window).on("load",function(){$(".loading").fadeOut(500)});
